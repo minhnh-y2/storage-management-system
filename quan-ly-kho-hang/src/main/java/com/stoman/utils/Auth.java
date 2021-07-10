938c9eb1f04d88240a2bb62a40f86131e0227ccc
Huy Nguyen <hnguyenq85@gmail.com>
2021-07-10 20:41:35 +0700

    them auth, msgbox, xdate and ximage

---
+++ quan-ly-kho-hang/src/main/java/com/stoman/utils/Auth.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.stoman.utils;
+
+import com.stoman.entity.NhanVien;
+
+
+
+/**
+ *
+ * @author Huy
+ */
+public class Auth {
+    public static NhanVien user = null;
+    
+    /**
+     * xoa bo nguoi dung hien tai
+     */
+    public static void clear(){
+        Auth.user = null;
+    }
+    
+    /**
+     * kiem tra dang nhap hay chua
+     * @return 
+     */
+    public static boolean isLogin(){
+        return Auth.user != null;
+    }
+    
+    /**
+     * kiem tra nguoi dung co phai Truong Phong
+     * @return 
+     */
+    public static boolean isManager(){
+        return Auth.isLogin() && user.isVaiTRo();
+    }
+}
---
+++ quan-ly-kho-hang/src/main/java/com/stoman/utils/XImages.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.stoman.utils;
+
+import java.awt.Image;
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Huy
+ */
+public class XImages {
+    
+    /**
+     * lay icon cua chuong trinh tu file
+     * @return icon cua chuong trinh
+     */
+    public static Image getAppIcon(){
+        URL url = XImages.class.getResource("/com/edusys/ui/images/fpt.png");
+        return new ImageIcon(url).getImage();
+    }
+    
+    /**
+     * Luu file vao duong dan
+     * @param src duong dan noi se luu file
+     */
+    public static void save(File src) {
+        File dst = new File("logos", src.getName());
+        if(!dst.getParentFile().exists()){
+            dst.getParentFile().mkdirs();
+        }
+        
+        try {
+            Path from = Paths.get(src.getAbsolutePath());
+            Path to = Paths.get(dst.getAbsolutePath());
+            Files.copy(from, to, StandardCopyOption.REPLACE_EXISTING);
+        } catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+    
+    /**
+     * Lay icon tu file da duoc chon
+     * @param fileName duong dan den file duoc chon
+     * @return icon
+     */
+    public static ImageIcon read(String fileName) {
+        File path = new File ("logos", fileName);
+        return new ImageIcon(path.getAbsolutePath());
+    }
+}
