ef0347d210208408cb8ff591c9621f47cfc4e0b9
Nguyen Hoai Minh <hoaiminh4321@gmail.com>
2021-07-10 20:33:01 +0700

    Create XSecurity.java

---
+++ quan-ly-kho-hang/src/main/java/com/stoman/utils/XSecurity.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.stoman.utils;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ *
+ * @author MinhNH
+ */
+public class XSecurity {
+
+    public static String getSalt() throws NoSuchAlgorithmException {
+        SecureRandom sr = new SecureRandom();
+        byte[] salt = new byte[16];
+        sr.nextBytes(salt);
+        return new String(salt, StandardCharsets.UTF_8);
+    }
+
+    /**
+     * Tạo muối theo độ dài chuỗi
+     */  
+    public static String generateSalt(int lenght) {
+        String abcCapitals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String abcLowerCase = "abcdefghijklmnopqrstuvwxyz";
+        String numbers = "01234567890123456789";
+        String characters = "!@#$%^&*!@#$%%^^&*";
+
+        String total = abcCapitals + abcLowerCase + numbers + characters;
+
+        StringBuilder sb = new StringBuilder();
+
+        char letters[] = new char[lenght];
+        for (int i = 0; i < lenght; i++) {
+            Random r = new Random();
+            char letter = total.charAt(r.nextInt(total.length()));
+            letters[i] = letter;
+        }
+        
+        sb.append(letters);
+
+        return sb.toString();
+    }
+
+    public static String getSecurePasswordSHA512(String password, String salt) {
+        String generatedPassword = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            md.update(salt.getBytes(StandardCharsets.UTF_8));
+            byte[] bytes = md.digest(password.getBytes(StandardCharsets.UTF_8));
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bytes.length; i++) {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            generatedPassword = sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return generatedPassword;
+    }
+    
+    public static boolean authenticate(String passwordInput, String passwordDB, String salt){
+        String password = getSecurePasswordSHA512(passwordInput, salt);
+        return password.equals(passwordDB);
+    }
+    
+}
