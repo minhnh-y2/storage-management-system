62f2860c72f5616cabd77bbb50831a3de3d3f553
Huy Nguyen <hnguyenq85@gmail.com>
2021-07-30 19:47:58 +0700

    cap nhat he thong

--- quan-ly-kho-hang/src/main/java/com/stoman/ui/PhieuNhapXuatKhoJDialog.java
+++ quan-ly-kho-hang/src/main/java/com/stoman/ui/PhieuNhapXuatKhoJDialog.java
@@ -492,8 +492,10 @@ public class PhieuNhapXuatKhoJDialog extends javax.swing.JDialog {
 
         txtNgayLap.setEditable(false);
         txtNgayLap.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.DateFormatter(new java.text.SimpleDateFormat("dd-MM-yyyy HH:mm:ss"))));
+        txtNgayLap.setDisabledTextColor(new java.awt.Color(0, 0, 0));
 
         txtMaNV.setEditable(false);
+        txtMaNV.setDisabledTextColor(new java.awt.Color(0, 0, 0));
 
         lblGhiChu.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
         lblGhiChu.setText("Ghi chú");
@@ -2063,7 +2065,7 @@ public class PhieuNhapXuatKhoJDialog extends javax.swing.JDialog {
             }
             ExporterReport.printReport(fileName, reportPath, parameters);
         } catch (SQLException | JRException e) {
-            MsgBox.alert(this, "Xuất file PDF thất bại!");
+            MsgBox.alert(this, "In phiếu thất bại!");
             e.printStackTrace();
         }
     }
